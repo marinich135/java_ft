6c57aaf9fbca74142ff7286b9cf44aaa979bf17a
Marysia <mspavlova@list.ru>
2020-03-15 22:48:17 +0300

    Сортировка списков.
    Лекция 4.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -13,7 +13,7 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -44,19 +44,6 @@ public class GroupData {
   public void setId(int id) {
     this.id = id;
   }
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
 
   @Override
   public String toString() {
@@ -65,4 +52,16 @@ public class GroupData {
             ", name='" + name + '\'' +
             '}';
   }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
 }
