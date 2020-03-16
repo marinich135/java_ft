9bd575a05647ef1b3073a2c1dc01f2be9976d403
Marysia <mspavlova@list.ru>
2020-03-16 12:39:47 +0300

    Добавлен тест для подсчёта количества элементов в коллекции для ContactModification
    Лекция 4.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,7 +9,7 @@ public class ContactModificationTests extends TestBase {
   public void testContactModification () {
 
     app.getContactHelper().gotoHomePage();
-
+    int before = app.getContactHelper().getContactCount();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().gotoCreateContactPage();
       app.getContactHelper().createContact(new ContactData("Ivan", "Sergeevich", "Petrov", "Frog", "TTI", "Moscow, Pobedy str", "89214567821", "ivanov@mail.ru", "test1"), true);
@@ -20,5 +21,7 @@ public class ContactModificationTests extends TestBase {
     app.getContactHelper().fillContactForm(new ContactData("Igor", "Vladimirovich","Kuzmenko","Izya","TOR","Moscow", "8952365843","Igoryan@mail.ru", null), false);
     app.getContactHelper().submitContactModifications();
     app.getContactHelper().gotoHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after,before);
   }
 }
