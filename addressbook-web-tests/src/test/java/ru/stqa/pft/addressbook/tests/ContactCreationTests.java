4485533543b3cbb47b485276b91824a4292f6a3d
Marysia <mspavlova@list.ru>
2020-03-16 13:48:01 +0300

    Добавлено сравнение списка контактов в ContactCreationTests
    Формирование коллекции, лекция 4.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,19 +5,21 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactCreationTests extends TestBase{
 
 
   @Test
   public void testContactCreation() throws Exception {
     app.getContactHelper().gotoHomePage();
-    int before = app.getContactHelper().getContactCount();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().gotoCreateContactPage();
     app.getContactHelper().fillContactForm(new ContactData("Ivan", "Sergeevich", "Petrov", "Frog", "TTI", "Moscow, Pobedy str", "89214567821", "ivanov@mail.ru", "test1"), true);
     app.getContactHelper().submitContactCreation();
     app.getContactHelper().gotoHomePage();
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after,before + 1);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(),before.size() + 1);
   }
 
 
