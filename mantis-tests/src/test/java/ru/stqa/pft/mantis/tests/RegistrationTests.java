c788e34e2526bc1c9d7d0f0528df5855725be856
Marysia <mspavlova@list.ru>
2020-06-06 11:21:01 +0300

    Лекция 8.7
    Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере (по протоколу Telnet)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class RegistrationTests extends TestBase {
-  @BeforeMethod
+  //@BeforeMethod
   public void startMailServer() {
     app.mail().start();
   }
@@ -21,13 +21,15 @@ public class RegistrationTests extends TestBase {
   @Test
   public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
-    String email = String.format("user%s@localhost.localdomain", now);
+    String email = String.format("user%s@localhost", now);
     String user = String.format ("user%s", now);
     String password = "password";
+    app.james().createUser(user, password);
     app.registration().start(user, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 600000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
-    app.registration().finish(confirmationLink, "root");
+    app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user));
 
   }
@@ -39,7 +41,7 @@ public class RegistrationTests extends TestBase {
   }
 
 
-  @AfterMethod(alwaysRun = true)
+  //@AfterMethod(alwaysRun = true)
   public void stopMailServer(){
     app.mail().stop();
   }
